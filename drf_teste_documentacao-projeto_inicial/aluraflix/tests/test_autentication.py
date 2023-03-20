57259c3c41675f84f0e0d9def36dd4596226a91b
Antônio Matheus Follone Graça <matheus-follone@follone.com.br>
2023-03-20 19:22:52 -0300

    Testes de integração
    Testes de integração

---
+++ drf_teste_documentacao-projeto_inicial/aluraflix/tests/test_autentication.py
@@ -0,0 +1,58 @@
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from django.contrib.auth import authenticate
+from django.urls import reverse
+from rest_framework import status
+
+class AuthenticationUserTestCase(APITestCase):
+    def setUp(self):
+        # o -list lista os endpoinst(rotas) de programas
+        self.list_url = reverse('programas-list')
+        # criando usuário para teste
+        self.user = User.objects.create_user(
+            'c3po',
+            password= '123456'
+        )
+    
+    def test_autenticacao_user_com_credenciais_corretas(self):
+        '''
+            teste que verifica a autentticao de um user com as credenciais corretas
+        '''
+        user = authenticate(username = 'c3po',  password= '123456')
+        # verificando  se o user está com valores e autenticado
+        self.assertTrue(user is not None) and user.is_authenticated
+    
+    def test_requisicao_get_nao_autorizada(self):
+        '''
+            teste que verifica uma requisição GET não autorizado 
+        '''
+        # fazendo uma requisição do tipo get
+        response =self.client.get(self.list_url)
+        # se o código de status é igual a 401(não autorizado)
+        self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
+
+    
+    def test_autenticacao_de_user_com_username_incorreto(self):
+        '''
+            Teste que verifica autenticação de um user com username incorreto
+        '''
+        # errando o nick 
+        user = authenticate(username = 'c3pp',  password= '123456')
+        # verificando  se o user está o username errado
+        self.assertFalse(user is not None) and user.is_authenticated
+    
+    def test_autenticacao_de_user_com_password_incorreto(self):
+        '''
+            Teste que verifica autenticação de um user com username incorreto
+        '''
+        # errando a senha 
+        user = authenticate(username = 'c3po',  password= '1234567')
+        # verificando  se o user está o username errado
+        self.assertFalse(user is not None) and user.is_authenticated
+
+    
+    def test_requisicao_get_com_user_autenticado(self):
+        '''
+            Teste que verifica uma requisição GET de um user autenticado 
+        '''
+    
