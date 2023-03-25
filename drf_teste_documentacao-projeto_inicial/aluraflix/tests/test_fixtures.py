c25fb73c4ccf6f6178efa07cc366b88abe115af1
Antônio Matheus Follone Graça <matheus-follone@follone.com.br>
2023-03-24 23:34:00 -0300

    Create test_fixtures.py

---
+++ drf_teste_documentacao-projeto_inicial/aluraflix/tests/test_fixtures.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from aluraflix.models import Programa
+
+class FixturesDataTestCase(TestCase):
+    # testando fixtures
+    # falando aonde está o arquivo de fixtures
+    fixtures = ['programas_iniciais']
+
+    def test_verifica_carregamento_da_fixtures(self):
+        programa_bizarro = Programa.objects.get(pk = 1)
+        todos_os_programas = Programa.objects.all()
+        self.assertEqual(programa_bizarro.titulo,"Coisas bizarras")
+        self.assertEqual(len(todos_os_programas),9)
